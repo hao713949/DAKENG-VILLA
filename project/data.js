913bd470f6d734167c945bf8c9fe7c6ea4917e4e
hao713949 <92414286+hao713949@users.noreply.github.com>
2026-04-16 23:07:47 +0800

    Update data.js

--- project/data.js
+++ project/data.js
@@ -1,26 +1,51 @@
 const unitDatabase = {
     "C1": {
         "id": "C1",
-        "authHash": "6f059086f6874f63c87e3760a5e8f3d6411516e87b7a6797a1518b577014457e",
+        "password": "1934",
         "title": "地號 487-007 ‧ 核心領袖單元",
-        "summary": { "price": "1,934.88", "unitPrice": "9.03", "totalArea": "214.22", "shareRatio": "1360 / 100,000" },
-        "visuals": { "droneView": "C1.png" },
-        "landDetails": [{ "id": "487-007", "private": "158.537", "public": "55.687", "total": "214.224" }]
+        "summary": {
+            "price": "1,934.88",
+            "unitPrice": "9.03",
+            "totalArea": "214.22",
+            "shareRatio": "1360 / 100,000"
+        },
+        "visuals": {
+            "droneView": "C1.png"
+        },
+        "landDetails": [
+            { "id": "487-007", "private": "158.537", "public": "55.687", "total": "214.224" }
+        ]
     },
     "C1-1": {
         "id": "C1-1",
-        "authHash": "509744c0631d8717d23f7335607b36f7344c803023e659b9c9f4d1e2612f0068",
+        "password": "1599",
         "title": "地號 487-008 ‧ 核心領袖單元",
-        "summary": { "price": "1,599.88", "unitPrice": "9.43", "totalArea": "169.62", "shareRatio": "1077 / 100,000" },
-        "visuals": { "droneView": "C1-1.png" },
-        "landDetails": [{ "id": "487-008", "private": "125.528", "public": "44.092", "total": "169.620" }]
+        "summary": {
+            "price": "1,599.88",
+            "unitPrice": "9.43",
+            "totalArea": "169.62",
+            "shareRatio": "1077 / 100,000"
+        },
+        "visuals": {
+            "droneView": "C1-1.png"
+        },
+        "landDetails": [
+            { "id": "487-008", "private": "125.528", "public": "44.092", "total": "169.620" }
+        ]
     },
     "C2": {
         "id": "C2",
-        "authHash": "634863738096f9a061488c0347898f060a0f5d72f5d96f96603a13768393998f",
+        "password": "2616",
         "title": "地號 487-020、487-021 ‧ 核心領袖單元",
-        "summary": { "price": "2,616.12", "unitPrice": "9.03", "totalArea": "289.65", "shareRatio": "1839 / 100,000" },
-        "visuals": { "droneView": "C2.png" },
+        "summary": {
+            "price": "2,616.12",
+            "unitPrice": "9.03",
+            "totalArea": "289.65",
+            "shareRatio": "1839 / 100,000"
+        },
+        "visuals": {
+            "droneView": "C2.png"
+        },
         "landDetails": [
             { "id": "487-020", "private": "152.227", "public": "53.470", "total": "205.697" },
             { "id": "487-021", "private": "62.128", "public": "21.822", "total": "83.950" }
@@ -28,10 +53,17 @@ const unitDatabase = {
     },
     "C2-1": {
         "id": "C2-1",
-        "authHash": "27838545e851996504a7428809ba099951152a55925e0161476378413264663c",
+        "password": "2219",
         "title": "地號 487-021-1、487-022 ‧ 核心領袖單元",
-        "summary": { "price": "2,219.16", "unitPrice": "9.03", "totalArea": "245.70", "shareRatio": "1560 / 100,000" },
-        "visuals": { "droneView": "C2-1.png" },
+        "summary": {
+            "price": "2,219.16",
+            "unitPrice": "9.03",
+            "totalArea": "245.70",
+            "shareRatio": "1560 / 100,000"
+        },
+        "visuals": {
+            "droneView": "C2-1.png"
+        },
         "landDetails": [
             { "id": "487-021-1", "private": "59.792", "public": "21.002", "total": "80.794" },
             { "id": "487-022", "private": "122.038", "public": "42.866", "total": "164.90" }
@@ -39,10 +71,17 @@ const unitDatabase = {
     },
     "C3": {
         "id": "C3",
-        "authHash": "18931139783f81507f352103328f1107ecb9308be7e5f18706316239f7e7506c",
+        "password": "2979",
         "title": "地號 487-023、487-024 ‧ 核心領袖單元",
-        "summary": { "price": "2,979.58", "unitPrice": "9.03", "totalArea": "329.89", "shareRatio": "2094 / 100,000" },
-        "visuals": { "droneView": "C3.png" },
+        "summary": {
+            "price": "2,979.58",
+            "unitPrice": "9.03",
+            "totalArea": "329.89",
+            "shareRatio": "2094 / 100,000"
+        },
+        "visuals": {
+            "droneView": "C3.png"
+        },
         "landDetails": [
             { "id": "487-023", "private": "122.709", "public": "43.102", "total": "165.81" },
             { "id": "487-024", "private": "121.427", "public": "42.651", "total": "164.078" }
@@ -50,10 +89,17 @@ const unitDatabase = {
     },
     "C5": {
         "id": "C5",
-        "authHash": "1001a41851e390c5c64c7ad078e63013b194519992f2545f4df2c5e5c7018318",
+        "password": "2376",
         "title": "地號 487-025、487-026 ‧ 核心領袖單元",
-        "summary": { "price": "2,376.73", "unitPrice": "9.03", "totalArea": "263.14", "shareRatio": "1670 / 100,000" },
-        "visuals": { "droneView": "C5.png" },
+        "summary": {
+            "price": "2,376.73",
+            "unitPrice": "9.03",
+            "totalArea": "263.14",
+            "shareRatio": "1670 / 100,000"
+        },
+        "visuals": {
+            "droneView": "C5.png"
+        },
         "landDetails": [
             { "id": "487-025", "private": "98.061", "public": "34.444", "total": "132.51" },
             { "id": "487-026", "private": "96.679", "public": "33.959", "total": "130.64" }
@@ -61,10 +107,17 @@ const unitDatabase = {
     },
     "C6": {
         "id": "C6",
-        "authHash": "97034c449174092b60cc88383c2670e9a56827083049da41846f481a5d6a2f81",
+        "password": "3586",
         "title": "地號 487-027、487-028、487-029 ‧ 隱境首席單元",
-        "summary": { "price": "3,586.71", "unitPrice": "8.83", "totalArea": "406.10", "shareRatio": "2578 / 100,000" },
-        "visuals": { "droneView": "C6.png" },
+        "summary": {
+            "price": "3,586.71",
+            "unitPrice": "8.83",
+            "totalArea": "406.10",
+            "shareRatio": "2578 / 100,000"
+        },
+        "visuals": {
+            "droneView": "C6.png"
+        },
         "landDetails": [
             { "id": "487-027", "private": "116.272", "public": "40.841", "total": "157.11" },
             { "id": "487-028", "private": "92.471", "public": "32.481", "total": "124.95" },
@@ -73,10 +126,17 @@ const unitDatabase = {
     },
     "C7": {
         "id": "C7",
-        "authHash": "60e1d13f98218903c14f09a56f2f2c8d281a8c9033486259068038b3a09796e6",
+        "password": "2208",
         "title": "地號 487-030、487-031 ‧ 隱境首席單元",
-        "summary": { "price": "2,208.76", "unitPrice": "8.83", "totalArea": "250.08", "shareRatio": "1588 / 100,000" },
-        "visuals": { "droneView": "C7.png" },
+        "summary": {
+            "price": "2,208.76",
+            "unitPrice": "8.83",
+            "totalArea": "250.08",
+            "shareRatio": "1588 / 100,000"
+        },
+        "visuals": {
+            "droneView": "C7.png"
+        },
         "landDetails": [
             { "id": "487-030", "private": "92.861", "public": "32.618", "total": "125.48" },
             { "id": "487-031", "private": "92.214", "public": "32.390", "total": "124.60" }
@@ -84,10 +144,17 @@ const unitDatabase = {
     },
     "C8": {
         "id": "C8",
-        "authHash": "959196b297b47b4260d778d9b158865675c93710776b7e64a13e618239a04a43",
+        "password": "2912",
         "title": "地號 487-032、487-033 ‧ 隱境首席單元",
-        "summary": { "price": "2,912.09", "unitPrice": "8.83", "totalArea": "329.72", "shareRatio": "2093 / 100,000" },
-        "visuals": { "droneView": "C8.png" },
+        "summary": {
+            "price": "2,912.09",
+            "unitPrice": "8.83",
+            "totalArea": "329.72",
+            "shareRatio": "2093 / 100,000"
+        },
+        "visuals": {
+            "droneView": "C8.png"
+        },
         "landDetails": [
             { "id": "487-032", "private": "121.944", "public": "42.833", "total": "164.78" },
             { "id": "487-033", "private": "122.065", "public": "42.876", "total": "164.94" }
@@ -95,10 +162,17 @@ const unitDatabase = {
     },
     "C9": {
         "id": "C9",
-        "authHash": "017830b804561a32997235552a420959b407481ecb550259b489d89260c6d57f",
+        "password": "2913",
         "title": "地號 487-034、487-035 ‧ 隱境首席單元",
-        "summary": { "price": "2,913.28", "unitPrice": "8.83", "totalArea": "329.85", "shareRatio": "2094 / 100,000" },
-        "visuals": { "droneView": "C9.png" },
+        "summary": {
+            "price": "2,913.28",
+            "unitPrice": "8.83",
+            "totalArea": "329.85",
+            "shareRatio": "2094 / 100,000"
+        },
+        "visuals": {
+            "droneView": "C9.png"
+        },
         "landDetails": [
             { "id": "487-034", "private": "121.777", "public": "42.775", "total": "164.55" },
             { "id": "487-035", "private": "122.331", "public": "42.969", "total": "165.30" }
@@ -106,10 +180,17 @@ const unitDatabase = {
     },
     "C10": {
         "id": "C10",
-        "authHash": "f5f569766d3f234f2d34608678096c13e5127027bc37943d0e376f9268f76634",
+        "password": "3614",
         "title": "地號 487-036、487-037 ‧ 隱境首席單元",
-        "summary": { "price": "3,614.55", "unitPrice": "8.83", "totalArea": "409.25", "shareRatio": "2598 / 100,000" },
-        "visuals": { "droneView": "C10.png" },
+        "summary": {
+            "price": "3,614.55",
+            "unitPrice": "8.83",
+            "totalArea": "409.25",
+            "shareRatio": "2598 / 100,000"
+        },
+        "visuals": {
+            "droneView": "C10.png"
+        },
         "landDetails": [
             { "id": "487-036", "private": "151.958", "public": "53.376", "total": "205.33" },
             { "id": "487-037", "private": "150.911", "public": "53.008", "total": "203.92" }
@@ -117,10 +198,17 @@ const unitDatabase = {
     },
     "D1": {
         "id": "D1",
-        "authHash": "b6a83163740e047537b0b14457ca67098e6d2460e527f30089e504c35634e06d",
+        "password": "2953",
         "title": "地號 487-056、487-057 ‧ 雲岫閱海單元*",
-        "summary": { "price": "2,953.10", "unitPrice": "8.65", "totalArea": "341.48", "shareRatio": "2168 / 100,000" },
-        "visuals": { "droneView": "D1.png" },
+        "summary": {
+            "price": "2,953.10",
+            "unitPrice": "8.65",
+            "totalArea": "341.48",
+            "shareRatio": "2168 / 100,000"
+        },
+        "visuals": {
+            "droneView": "D1.png"
+        },
         "landDetails": [
             { "id": "487-056", "private": "178.466", "public": "62.687", "total": "241.15" },
             { "id": "487-057", "private": "74.249", "public": "26.080", "total": "100.33" }
@@ -128,10 +216,17 @@ const unitDatabase = {
     },
     "D1-1": {
         "id": "D1-1",
-        "authHash": "70d10041d8b9f697525e83e9b0851ec04262bc61e7a57c503b41728362629676",
+        "password": "2640",
         "title": "地號 487-057-1、487-058 ‧ 雲岫閱海單元*",
-        "summary": { "price": "2,640.51", "unitPrice": "8.65", "totalArea": "305.34", "shareRatio": "1938 / 100,000" },
-        "visuals": { "droneView": "D1-1.png" },
+        "summary": {
+            "price": "2,640.51",
+            "unitPrice": "8.65",
+            "totalArea": "305.34",
+            "shareRatio": "1938 / 100,000"
+        },
+        "visuals": {
+            "droneView": "D1-1.png"
+        },
         "landDetails": [
             { "id": "487-057-1", "private": "75.386", "public": "26.480", "total": "101.87" },
             { "id": "487-058", "private": "150.578", "public": "52.891", "total": "203.47" }
@@ -139,10 +234,17 @@ const unitDatabase = {
     },
     "D2": {
         "id": "D2",
-        "authHash": "3f421f584489a24c160c1d1a93b4a28f80453303c7344933a2707f45a0ed79e8",
+        "password": "2238",
         "title": "地號 487-059、487-060 ‧ 雲岫閱海單元*",
-        "summary": { "price": "2,238.64", "unitPrice": "9.00", "totalArea": "248.80", "shareRatio": "1579 / 100,000" },
-        "visuals": { "droneView": "D2.png" },
+        "summary": {
+            "price": "2,238.64",
+            "unitPrice": "9.00",
+            "totalArea": "248.80",
+            "shareRatio": "1579 / 100,000"
+        },
+        "visuals": {
+            "droneView": "D2.png"
+        },
         "landDetails": [
             { "id": "487-059", "private": "91.751", "public": "32.228", "total": "123.98" },
             { "id": "487-060", "private": "92.371", "public": "32.446", "total": "124.82" }
@@ -150,10 +252,17 @@ const unitDatabase = {
     },
     "H1": {
         "id": "H1",
-        "authHash": "c05561a2936a282436d6df593a10959f237580667d4039b7a42a8b9e6918d360",
+        "password": "2178",
         "title": "地號 487-102、487-103 ‧ 巔峰視野單元",
-        "summary": { "price": "2,178.4", "unitPrice": "8.83", "totalArea": "246.65", "shareRatio": "1566 / 100,000" },
-        "visuals": { "droneView": "H1.png" },
+        "summary": {
+            "price": "2,178.4",
+            "unitPrice": "8.83",
+            "totalArea": "246.65",
+            "shareRatio": "1566 / 100,000"
+        },
+        "visuals": {
+            "droneView": "H1.png"
+        },
         "landDetails": [
             { "id": "487-102", "private": "92.018", "public": "32.321", "total": "124.34" },
             { "id": "487-103", "private": "90.514", "public": "31.793", "total": "122.31" }
@@ -161,10 +270,17 @@ const unitDatabase = {
     },
     "H2": {
         "id": "H2",
-        "authHash": "933a2862d51249b6b66974751761e05f69c0d66c243666f289d0689b9f485121",
+        "password": "2201",
         "title": "地號 487-104、487-105 ‧ 巔峰視野單元",
-        "summary": { "price": "2,201.93", "unitPrice": "8.83", "totalArea": "249.31", "shareRatio": "1583 / 100,000" },
-        "visuals": { "droneView": "H2.png" },
+        "summary": {
+            "price": "2,201.93",
+            "unitPrice": "8.83",
+            "totalArea": "249.31",
+            "shareRatio": "1583 / 100,000"
+        },
+        "visuals": {
+            "droneView": "H2.png"
+        },
         "landDetails": [
             { "id": "487-104", "private": "92.060", "public": "32.336", "total": "124.4" },
             { "id": "487-105", "private": "92.440", "public": "32.471", "total": "124.92" }
@@ -172,10 +288,17 @@ const unitDatabase = {
     },
     "H3": {
         "id": "H3",
-        "authHash": "27f00693a9e403487c06f15715e4f71a7a0b3f54546571577d6f519f71c322f2",
+        "password": "2190",
         "title": "地號 487-106、487-107 ‧ 巔峰視野單元",
-        "summary": { "price": "2,190.42", "unitPrice": "8.83", "totalArea": "248.01", "shareRatio": "1574 / 100,000" },
-        "visuals": { "droneView": "H3.png" },
+        "summary": {
+            "price": "2,190.42",
+            "unitPrice": "8.83",
+            "totalArea": "248.01",
+            "shareRatio": "1574 / 100,000"
+        },
+        "visuals": {
+            "droneView": "H3.png"
+        },
         "landDetails": [
             { "id": "487-106", "private": "91.706", "public": "32.212", "total": "123.92" },
             { "id": "487-107", "private": "91.833", "public": "32.257", "total": "124.09" }
@@ -183,10 +306,17 @@ const unitDatabase = {
     },
     "H10": {
         "id": "H10",
-        "authHash": "205930064971f11c750b691079bc75841054002e264583162799c33433a00508",
+        "password": "2349",
         "title": "地號 487-136、487-137 ‧ 森態綠意單元",
-        "summary": { "price": "2,349.28", "unitPrice": "8.45", "totalArea": "278.09", "shareRatio": "1765 / 100,000" },
-        "visuals": { "droneView": "H10.png" },
+        "summary": {
+            "price": "2,349.28",
+            "unitPrice": "8.45",
+            "totalArea": "278.09",
+            "shareRatio": "1765 / 100,000"
+        },
+        "visuals": {
+            "droneView": "H10.png"
+        },
         "landDetails": [
             { "id": "487-136", "private": "105.899", "public": "37.197", "total": "143.10" },
             { "id": "487-137", "private": "99.901", "public": "35.090", "total": "134.99" }
@@ -194,10 +324,17 @@ const unitDatabase = {
     },
     "E5": {
         "id": "E5",
-        "authHash": "1000b0f796d83a9366df0410425c2759e51c8816c7a7605658097b377b63f2f8",
+        "password": "2780",
         "title": "地號 487-175、487-176 ‧ 層峰聚落單元",
-        "summary": { "price": "2,780.79", "unitPrice": "8.57", "totalArea": "324.48", "shareRatio": "2060 / 100,000" },
-        "visuals": { "droneView": "E5.png" },
+        "summary": {
+            "price": "2,780.79",
+            "unitPrice": "8.57",
+            "totalArea": "324.48",
+            "shareRatio": "2060 / 100,000"
+        },
+        "visuals": {
+            "droneView": "E5.png"
+        },
         "landDetails": [
             { "id": "487-175", "private": "121.986", "public": "42.842", "total": "164.81" },
             { "id": "487-176", "private": "118.163", "public": "41.505", "total": "159.67" }
@@ -205,10 +342,17 @@ const unitDatabase = {
     },
     "E6": {
         "id": "E6",
-        "authHash": "000288879555c9118c734181f0ef69741e988229b13c72b535899882944b584d",
+        "password": "2706",
         "title": "地號 487-177、487-178 ‧ 層峰聚落單元",
-        "summary": { "price": "2,706.77", "unitPrice": "8.57", "totalArea": "315.84", "shareRatio": "2005 / 100,000" },
-        "visuals": { "droneView": "E6.png" },
+        "summary": {
+            "price": "2,706.77",
+            "unitPrice": "8.57",
+            "totalArea": "315.84",
+            "shareRatio": "2005 / 100,000"
+        },
+        "visuals": {
+            "droneView": "E6.png"
+        },
         "landDetails": [
             { "id": "487-177", "private": "117.821", "public": "41.385", "total": "159.21" },
             { "id": "487-178", "private": "115.918", "public": "40.716", "total": "156.63" }
@@ -216,10 +360,17 @@ const unitDatabase = {
     },
     "E7": {
         "id": "E7",
-        "authHash": "074249a2a9058b76c8c505470550c6046e7379658074906d4e89920b784e1b82",
+        "password": "2859",
         "title": "地號 487-179、487-180 ‧ 層峰聚落單元",
-        "summary": { "price": "2,859.85", "unitPrice": "8.57", "totalArea": "333.70", "shareRatio": "2118 / 100,000" },
-        "visuals": { "droneView": "E7.png" },
+        "summary": {
+            "price": "2,859.85",
+            "unitPrice": "8.57",
+            "totalArea": "333.70",
+            "shareRatio": "2118 / 100,000"
+        },
+        "visuals": {
+            "droneView": "E7.png"
+        },
         "landDetails": [
             { "id": "487-179", "private": "115.458", "public": "40.555", "total": "156.01" },
             { "id": "487-180", "private": "131.50", "public": "46.190", "total": "177.69" }
